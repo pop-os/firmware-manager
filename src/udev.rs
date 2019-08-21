d7f30a60f2f78759386965595a4011eb262e3cfd
Michael Aaron Murphy <mmstick@pm.me>
2019-08-20 22:40:39 -0600

    feat(core): Callers may now stop the USB hotplug event thread
    Returns a `Trigger` value from the `usb_hotplug_event_loop` function.
    When this value is dropped, it will stop further execution of the
    `Stream` running in the background thread which was spawned by this
    function. Thus, the caller will be able to stop this thread at will.

--- src/udev.rs
+++ src/udev.rs
@@ -1,35 +1,41 @@
 use futures::{stream::Stream, Future};
 use std::thread;
+use stream_cancel::{Trigger, Valved};
 use tokio_udev::{Context, Event, EventType, MonitorBuilder};
 
 macro_rules! ok_or_return {
     ($expression:expr) => {
         match $expression {
             Ok(value) => value,
-            Err(_) => return,
+            Err(_) => return None,
         }
     };
 }
 
 /// Convenience function for an event loop which reacts to USB hotplug events.
-pub fn usb_hotplug_event_loop<F: Fn() + Send + 'static>(func: F) {
+pub fn usb_hotplug_event_loop<F: Fn() + Send + 'static>(func: F) -> Option<Trigger> {
+    trace!("initiating USB hotplug event loop thread");
+
+    let context = ok_or_return!(Context::new());
+    let mut builder = ok_or_return!(MonitorBuilder::new(&context));
+    ok_or_return!(builder.match_subsystem_devtype("usb", "usb_device"));
+    let monitor = ok_or_return!(builder.listen());
+
+    let handler = move |e: Event| {
+        match e.event_type() {
+            EventType::Add | EventType::Remove => func(),
+            _ => (),
+        }
+        Ok(())
+    };
+
+    let (triggered, stream) = Valved::new(monitor);
+
     thread::spawn(move || {
-        trace!("initiating USB hotplug event loop thread");
-
-        let context = ok_or_return!(Context::new());
-        let mut builder = ok_or_return!(MonitorBuilder::new(&context));
-        ok_or_return!(builder.match_subsystem_devtype("usb", "usb_device"));
-        let monitor = ok_or_return!(builder.listen());
-
-        let handler = move |e: Event| {
-            match e.event_type() {
-                EventType::Add | EventType::Remove => func(),
-                _ => (),
-            }
-            Ok(())
-        };
-
         trace!("USB hotplug events now being processed");
-        tokio::run(monitor.for_each(handler).map_err(|_| ()));
+        tokio::run(stream.for_each(handler).map_err(|_| ()));
+        trace!("usb hotplug thread stopped");
     });
+
+    Some(triggered)
 }
